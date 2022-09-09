f8d1ea723f80b5fed631df09bc9a1f243a90c0ea
GiCharkviani <gcharkvi@broadinstitute.org>
2022-09-09 21:34:05 +0400

    SSR Done

--- 12. Universal (SSR)/angular-ssr/src/app/app.server.module.ts
+++ 12. Universal (SSR)/angular-ssr/src/app/app.server.module.ts
@@ -3,11 +3,19 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+
+const routes: Routes = [
+  {path:'home', component: AppComponent},
+  {path:'login', component: LoginComponent}
+]
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent],
 })
