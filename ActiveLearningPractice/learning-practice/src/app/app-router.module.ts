ec0e1a8ae0d9ecdc092049095e919559b270867f
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-22 18:53:00 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
+++ ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
@@ -1,10 +1,5 @@
-import {Route, RouterModule} from '@angular/router';
-import {ModuleWithProviders} from "@angular/core";
+import {Route} from '@angular/router';
 
 export const routes: Route[] = [
-  {
-    path: 'item/:id',
-    title: 'Item clicked',
-    loadComponent: () => import("./item/item.component").then(m => m.ItemComponent)},
 ];
 
--- ActiveLearningPractice/learning-practice/src/app/app.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/app.component.ts
@@ -1,28 +1,12 @@
 import {
-  afterRender,
   ChangeDetectionStrategy,
-  Component, computed, effect, EffectCleanupRegisterFn, EffectRef, inject, Inject, Injector,
-  OnInit,
-  Signal,
-  signal, untracked,
-  WritableSignal
+  Component,
 } from '@angular/core';
-import * as _ from 'lodash'
-import {map, Observable, shareReplay, Subject, tap} from "rxjs";
-import {toSignal} from "@angular/core/rxjs-interop";
 import {RouterLink, RouterOutlet} from "@angular/router";
-import {ButtonComponent} from "./button/button.component";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MyServiceService} from "./my-service.service";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {ItemComponent} from "./item/item.component";
 
-export interface Post {
-  id: number;
-  userId: number;
-  body: string;
-  title: string;
-}
+
 
 @Component({
   selector: 'app-root',
@@ -33,63 +17,11 @@ export interface Post {
     CommonModule,
     RouterLink,
     RouterOutlet,
-    ButtonComponent,
     MatProgressSpinnerModule,
-    ItemComponent,
     NgOptimizedImage
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
-  title!: string;
-  body!: string;
-  showMe: boolean = false;
+export class AppComponent  {
 
-  mySignal: WritableSignal<number> = signal(3, {equal: _.isEqual});
-  secondSignal: WritableSignal<number> = signal(5);
-
-  computerSignal: Signal<number> = computed(() => this.mySignal() + 2);
-  effect: EffectRef = effect(() => console.log(`Signal from effect - ${this.computerSignal()} -> ${this.mySignal()}`))
-
-  /* Observable to Signal */
-  myObs = new Subject<string>();
-  obsToSignal = toSignal(this.myObs, {initialValue: "Giorgi"});
-
-  private readonly myService = inject(MyServiceService);
-  public myData$: Observable<any[]> = this.myService.getData();
-
-  constructor(private readonly injector: Injector) {
-  }
-
-  ngOnInit() {
-    effect((onCleanup: EffectCleanupRegisterFn) => {
-      const mySignal = this.mySignal;
-      console.log('Second signal ' + this.secondSignal() + ' * ' + untracked(mySignal));
-
-      // const timer = setInterval(() => {
-      //   console.log('My Signal from timeout ' + mySignal())
-      // }, 1000)
-
-      // onCleanup(() => clearTimeout(timer))
-    }, {injector: this.injector});
-  }
-
-  setSignal(value: string): void {
-    this.mySignal.set(Number(value))
-  }
-
-  updateSignal(value: string): void {
-    this.mySignal.update((oldVal) => Number(value) * oldVal);
-    this.secondSignal.update((oldVal) => Number(value) / oldVal);
-  }
-
-  emitSubject(value: string): void {
-    this.myObs.next(value);
-    this.myObs.error({msg: 'Error'})
-  }
-
-  public todos(): Observable<any[]> {
-    return this.myService.getData()
-      .pipe(tap(console.log), shareReplay(1));
-  }
 }
