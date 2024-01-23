89e5b56d56a4a0f6fc35e5315c814d4bcaa90eb7
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-23 20:12:20 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/app.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/app.component.ts
@@ -1,12 +1,33 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit,} from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {Person} from "./decoratos/demo";
 
+@Component({
+  selector: 'testComponent',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <h2>Items: </h2>
+    <ul>
+      <li>Name: {{item.name}}</li>
+      <li>Quantity: {{item.quantity}}</li>
+    </ul>
+  `,
+  styles: ``,
+  standalone: true
+})
+export class testComponent implements OnInit, OnDestroy {
+  @Input() item: any;
+
+  ngOnInit() {
+    console.log(this.item.name + ' INITIALIZED')
+  }
+
+  ngOnDestroy() {
+    console.log(this.item.name + ' DESTROYED')
+  }
+
+}
 
 
 @Component({
@@ -19,7 +40,8 @@ import {Person} from "./decoratos/demo";
     RouterLink,
     RouterOutlet,
     MatProgressSpinnerModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    testComponent
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -29,7 +51,28 @@ export class AppComponent  {
 
   public render: boolean = false;
 
-  constructor() {
+  public ingredientList = [
+    {name: 'noodles', quantity: 1, id: 21},
+    {name: 'miso broth', quantity: 1,  id: 24},
+    {name: 'egg', quantity: 2,  id: 51},
+  ];
+
+  public addItem() {
+    this.ingredientList.push({
+      name: 'Khachapuri',
+      quantity: 32,
+      id: 54
+    })
+  }
+
+  public removeItem() {
+    this.ingredientList.pop()
+  }
+
+  getValue(name: string) {
+    console.log(name + ' - ITEM_CREATED')
+    return ''
   }
 
 }
+
