edac69aa002408fc4f23b90000af3a1458312228
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-24 20:41:29 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/components/styles/stylesTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/components/styles/stylesTester.component.ts
@@ -1,12 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
 
 @Component({
   selector: 'app-styles',
   standalone: true,
   imports: [],
   template: `
-    
-  `
+        <h1>My styles</h1>
+  `,
+  styles: `
+    h1 {
+    color: red;
+    }
+  `,
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class StylesTesterComponent {
 
