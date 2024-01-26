52ddfecf6a03691bc3a2053d7ddc973a6e2e54bd
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-26 17:58:13 +0100

    dsada

---
+++ ActiveLearningPractice/learning-practice/src/app/components/inputProperties/inputPropertiesTester.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-inputProperties',
+  standalone: true,
+  imports: [],
+  template: `
+        <h1>My styles</h1>
+  `,
+  styles: `
+  `
+})
+export class InputPropertiesTesterComponent {
+
+}
