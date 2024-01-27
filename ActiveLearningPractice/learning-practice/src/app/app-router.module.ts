975e6ab52f98dc6f0d7b5ef23dc8ad5188f1d00e
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-27 12:01:57 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
+++ ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
@@ -2,10 +2,12 @@ import {Route} from '@angular/router';
 import {SelectorsTesterComponent} from "./components/selectors/selectorsTester.component";
 import {StylesTesterComponent} from "./components/styles/stylesTester.component";
 import {InputPropertiesTesterComponent} from "./components/inputProperties/inputPropertiesTester.component";
+import {OutputPropertiesTesterComponent} from "./components/outputProperties/outputPropertiesTester.component";
 
 export const routes: Route[] = [
   {path: 'selector', component: SelectorsTesterComponent},
   {path: 'styles', component: StylesTesterComponent},
-  {path: 'inputProperties', component: InputPropertiesTesterComponent}
+  {path: 'inputProperties', component: InputPropertiesTesterComponent},
+  {path: 'outputProperties', component: OutputPropertiesTesterComponent}
 ];
 
---
+++ ActiveLearningPractice/learning-practice/src/app/components/outputProperties/outputPropertiesTester.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from "@angular/core";
+
+
+@Component({
+  selector: 'app-inputProperties',
+  standalone: true,
+  imports: [],
+  template: `
+
+  `,
+  styles: ``
+})
+export class OutputPropertiesTesterComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
