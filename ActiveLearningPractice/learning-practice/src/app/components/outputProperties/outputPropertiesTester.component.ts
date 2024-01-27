07e18c95766f294b5cbd69366a106a88853b8ad9
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-27 12:11:08 +0100

    DSADA

--- ActiveLearningPractice/learning-practice/src/app/components/outputProperties/outputPropertiesTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/components/outputProperties/outputPropertiesTester.component.ts
@@ -6,7 +6,11 @@ import {Component, OnInit} from "@angular/core";
   standalone: true,
   imports: [],
   template: `
-
+        <div (click)="onClick($event)">
+          <ul>
+            <li (click)="onClick($event)">Item</li>
+          </ul>
+        </div>
   `,
   styles: ``
 })
@@ -15,4 +19,9 @@ export class OutputPropertiesTesterComponent implements OnInit {
   ngOnInit() {
   }
 
+  onClick(event: any) {
+    console.log(event)
+    console.log(event.eventPhase, 'PHASE')
+    // event.cancelBubble = true;
+  }
 }
