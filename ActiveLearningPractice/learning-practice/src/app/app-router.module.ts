7d30135708e4aefb91c0154b0961699fbe5c13bc
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-27 18:35:45 +0100

    sdada

--- ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
+++ ActiveLearningPractice/learning-practice/src/app/app-router.module.ts
@@ -4,12 +4,14 @@ import {StylesTesterComponent} from "./components/styles/stylesTester.component"
 import {InputPropertiesTesterComponent} from "./components/inputProperties/inputPropertiesTester.component";
 import {OutputPropertiesTesterComponent} from "./components/outputProperties/outputPropertiesTester.component";
 import {ContentProjectionTesterComponent} from "./components/contentProjection/contentProjectionTester.component";
+import {HostElementsComponent} from "./components/hostElements/hostElements.component";
 
 export const routes: Route[] = [
   {path: 'selector', component: SelectorsTesterComponent},
   {path: 'styles', component: StylesTesterComponent},
   {path: 'inputProperties', component: InputPropertiesTesterComponent},
   {path: 'outputProperties', component: OutputPropertiesTesterComponent},
-  {path: 'contentProjection', component: ContentProjectionTesterComponent}
+  {path: 'contentProjection', component: ContentProjectionTesterComponent},
+  {path: 'hostElements', component: HostElementsComponent}
 ];
 
