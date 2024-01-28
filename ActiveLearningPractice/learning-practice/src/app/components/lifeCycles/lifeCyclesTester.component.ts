983111cad9feaf8cce7bf4b31739a88572bd03ce
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-28 21:18:30 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/components/lifeCycles/lifeCyclesTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/components/lifeCycles/lifeCyclesTester.component.ts
@@ -1,16 +1,51 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+
+
+@Component({
+  selector: 'app-localComponent',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>
+      <p>Name: {{name}}</p>
+      <p>Age: {{age}}</p>
+    </div>
+  `,
+  styles: ``
+})
+export class localComponent implements OnChanges {
+  @Input() name!: string;
+  @Input() age: number = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes, 'ON_CHANGES')
+  }
+
+}
+
 
 @Component({
   selector: 'app-lifeCycles',
   standalone: true,
-  imports: [],
-  template: ``,
+  imports: [
+    FormsModule,
+    localComponent
+  ],
+  template: `
+    <input type="text" [(ngModel)]="name" />
+    <app-localComponent [age]="23" [name]="name" ></app-localComponent>
+  `,
   styles: ``
 })
 export class LifeCyclesTesterComponent implements OnInit {
+  public name!: string;
 
   ngOnInit() {
     console.log('ON_INIT')
   }
 
 }
+
+
