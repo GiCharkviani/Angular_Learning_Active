e6a4b784d38d373a5f8b7aa9e2b04c5d881fd86b
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-28 21:05:57 +0100

    dsada

--- ActiveLearningPractice/learning-practice/src/app/components/lifeCycles/lifeCyclesTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/components/lifeCycles/lifeCyclesTester.component.ts
@@ -3,14 +3,10 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-lifeCycles',
   standalone: true,
-  imports: [
-
-  ],
-  template: `
-  `,
+  imports: [],
+  template: ``,
   styles: ``
 })
-export class LifeCyclesTesterComponent {
-
+export class LifeCyclesTesterComponent  {
 
 }
