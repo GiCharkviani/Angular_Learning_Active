bdf4e7fab5e3ff8b7c9dd7b3e878a31100606bbf
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-29 19:07:28 +0100

    dsadada

---
+++ ActiveLearningPractice/learning-practice/src/app/components/childrenQueries/childQuery.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-childQuery',
+  standalone: true,
+  imports: [
+
+  ],
+  template: `
+
+  `,
+  styles: ``
+})
+export class ChildQueryComponent {
+
+
+}
