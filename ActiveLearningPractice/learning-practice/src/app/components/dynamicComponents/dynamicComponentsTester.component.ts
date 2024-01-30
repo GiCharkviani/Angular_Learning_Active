2406c723b23c93bdfe1959ab5f29d043fb6fac40
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-30 17:40:51 +0100

    fasdada

--- ActiveLearningPractice/learning-practice/src/app/components/dynamicComponents/dynamicComponentsTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/components/dynamicComponents/dynamicComponentsTester.component.ts
@@ -1,29 +1,38 @@
 import {Component} from "@angular/core";
+import {NgComponentOutlet, NgTemplateOutlet} from "@angular/common";
+import {DynamicComponent} from "./dynamic.component";
 
 
 @Component({
   selector: 'app-dynamicComponentsTester',
   standalone: true,
   imports: [
-
+    NgTemplateOutlet,
+    NgComponentOutlet
   ],
   template: `
 
     <h1>Dynamic Components</h1>
+
     <h3>Template Outlet</h3>
+    <ng-container *ngTemplateOutlet="avto; context: {name: 'Warsaw'}"></ng-container>
 
+    <hr>
     <h3>Component Outlet</h3>
+    <ng-container *ngComponentOutlet="dynamic"></ng-container>
 
+    <hr>
     <h3>View Container Ref</h3>
 
     <!--  Template  -->
-    <ng-template>
+    <ng-template #avto let-cityName="name">
       <p>I am a ng-template</p>
+      <p>Name is: {{cityName}}</p>
     </ng-template>
   `,
   styles: ``
 })
 export class DynamicComponentsTesterComponent {
-
+  public dynamic = DynamicComponent;
 
 }
