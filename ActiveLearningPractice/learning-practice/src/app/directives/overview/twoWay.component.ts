e449968429b30ca631576b7ccea91d1d26855d6f
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-01 19:26:27 +0100

    fsdfs

---
+++ ActiveLearningPractice/learning-practice/src/app/directives/overview/twoWay.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-twoWay',
+  standalone: true,
+  imports: [
+  ],
+  template: `
+  `,
+  styles: ``
+})
+export class TwoWayComponent {
+
+}
