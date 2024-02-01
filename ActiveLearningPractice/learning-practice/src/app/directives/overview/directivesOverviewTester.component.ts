15359b271852b9f1e562ac7fcc153223bcb29db1
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-01 19:35:16 +0100

    fgdgd

--- ActiveLearningPractice/learning-practice/src/app/directives/overview/directivesOverviewTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/directives/overview/directivesOverviewTester.component.ts
@@ -1,20 +1,25 @@
 import {Component} from "@angular/core";
 import {FormsModule} from "@angular/forms";
+import {TwoWayComponent} from "./twoWay.component";
 
 @Component({
   selector: 'app-directivesOverview',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    TwoWayComponent
   ],
   template: `
     <input [(ngModel)]="name" type="text">
     <h4>Parent name: {{name}}</h4>
 
+    <app-twoWay [(name)]="childName"></app-twoWay>
+    <h4>Child name in Parent: {{childName}}</h4>
   `,
   styles: ``
 })
 export class DirectivesOverviewTesterComponent {
   public name!: string;
+  public childName!: string;
 
 }
--- ActiveLearningPractice/learning-practice/src/app/directives/overview/twoWay.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/directives/overview/twoWay.component.ts
@@ -1,14 +1,20 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-twoWay',
   standalone: true,
   template: `
-    <p>Name: {{name}}</p>
+    <p>Name in child: {{ name }}</p>
+    <input #nameInput (input)="nameChange.emit(nameInput.value)" type="text">
   `,
+  imports: [
+    ReactiveFormsModule
+  ],
   styles: ``
 })
 export class TwoWayComponent {
   @Input() name!: string;
+  @Output() nameChange = new EventEmitter();
 
 }
