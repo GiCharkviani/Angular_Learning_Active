e754ef6607243346d486165b3e3677250089a8ae
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-03 17:25:05 +0100

    fadfasda

--- ActiveLearningPractice/learning-practice/src/app/directives/structuralDirectives/structural.directive.ts
+++ ActiveLearningPractice/learning-practice/src/app/directives/structuralDirectives/structural.directive.ts
@@ -1,14 +1,18 @@
-import {Directive, ElementRef, Input, TemplateRef, ViewContainerRef} from "@angular/core";
+import {Directive, ElementRef, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef} from "@angular/core";
 
 @Directive({
   standalone: true,
   selector: '[gamoChndes]'
 })
-export class StructuralDirective {
+export class StructuralDirective implements OnChanges {
 
   @Input({required: true}) gamoChndes!: boolean;
 
   constructor(private templateRef: TemplateRef<any>, private elRef: ElementRef, private viewContainerRef: ViewContainerRef) {
     console.log(templateRef, viewContainerRef, elRef, 'ELEMENT-ATTACHED')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes, 'CHANGES')
   }
 }
