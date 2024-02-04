cb4a8178c3d1654327c8274959ec0df021b045b5
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-04 10:57:40 +0100

    fdsfs

---
+++ ActiveLearningPractice/learning-practice/src/app/DI/diTester.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-inputProperties',
+  standalone: true,
+  imports: [],
+  template: `
+
+  `,
+  styles: ``
+})
+export class DiTesterComponent {
+
+}
