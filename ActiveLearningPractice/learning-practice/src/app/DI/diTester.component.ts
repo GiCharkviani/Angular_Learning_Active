cfd46fd8f734c3065204619ad355fc2d248e3497
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-04 13:39:30 +0100

    gfdgd

--- ActiveLearningPractice/learning-practice/src/app/DI/diTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/DI/diTester.component.ts
@@ -1,6 +1,4 @@
-import {Component, Inject} from "@angular/core";
-import {LoggerService} from "./logger.service";
-import {BetterLoggerService} from "./betterLogger.service";
+import {Component} from "@angular/core";
 import {UserService} from "./user.service";
 
 
