3df9883cf902a8f4ac071b116c74399807067841
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-06 19:07:43 +0100

    gfdgfdg

---
+++ ActiveLearningPractice/learning-practice/src/app/DI/content.component.ts
@@ -0,0 +1,20 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-contentComponent',
+  standalone: true,
+  imports: [],
+  template: `
+        <p>I am Content Component</p>
+  `,
+  styles: ``,
+  providers: [
+
+  ]
+})
+export class DiChildComponent {
+  constructor() {
+
+  }
+}
