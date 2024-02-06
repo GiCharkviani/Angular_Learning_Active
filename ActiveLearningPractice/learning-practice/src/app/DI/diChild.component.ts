630962c5567f2b0be25e38f120fe76f31c699ba9
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-06 19:19:41 +0100

    gfdgd

--- ActiveLearningPractice/learning-practice/src/app/DI/diChild.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/DI/diChild.component.ts
@@ -1,6 +1,7 @@
-import {Component, Host, Optional, Self, SkipSelf} from "@angular/core";
+import {Component, Host, Inject, Optional, Self, SkipSelf} from "@angular/core";
 import {AuthService} from "./auth.service";
 import {LoggerService} from "./logger.service";
+import {FLOWER} from "./diTester.component";
 
 
 @Component({
@@ -8,7 +9,7 @@ import {LoggerService} from "./logger.service";
   standalone: true,
   imports: [],
   template: `
-        <p>I am child DI</p>
+        <p>I am child DI: {{flower.emojy}}</p>
         <ng-content></ng-content>
   `,
   styles: ``,
@@ -20,6 +21,7 @@ export class DiChildComponent {
   constructor(@Self() @Optional() private authService: AuthService,
               @SkipSelf() @Optional() private loggerService: LoggerService,
                // @Host() private betterLogger: BetterLoggerService
+              @Inject(FLOWER) public flower: {emojy: string}
               ) {
     console.log(this.authService?.userStatus());
     this.loggerService?.log();
