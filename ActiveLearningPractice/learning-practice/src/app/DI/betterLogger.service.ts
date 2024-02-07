d9b36f3dc7e458de56879cba02e604aaefd1dfdc
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-07 19:05:10 +0100

    gfdgd

--- ActiveLearningPractice/learning-practice/src/app/DI/betterLogger.service.ts
+++ ActiveLearningPractice/learning-practice/src/app/DI/betterLogger.service.ts
@@ -1,12 +1,8 @@
 import {Injectable} from "@angular/core";
-import {LoggerService} from "./logger.service";
 
 @Injectable()
-export class BetterLoggerService extends LoggerService {
+export class BetterLoggerService  {
 
-  override log() {
-    console.log('BETTER LOGGER')
-  }
 
   public logWarm() {
     console.warn('Something happens bro')
