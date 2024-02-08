1a5dbcca4cdd383319f435beee0872456769c239
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-08 14:09:25 +0100

    fsdfsf

--- ActiveLearningPractice/learning-practice/src/app/signals/signalState.service.ts
+++ ActiveLearningPractice/learning-practice/src/app/signals/signalState.service.ts
@@ -1,9 +1,15 @@
-import {Injectable, signal} from "@angular/core";
+import {computed, Injectable, signal} from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 export class SignalStateService {
   /* Writable Signals */
   public names = signal('');
+
+  /* Readable Signals */
+  public namesAndSurnames = computed(() => {
+    const localNames = this.names();
+    return localNames + ' Chavchavadze';
+  })
 
   public setNames(value: string) {
     this.names.set(value);
--- ActiveLearningPractice/learning-practice/src/app/signals/signalsTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/signals/signalsTester.component.ts
@@ -11,9 +11,10 @@ import {SignalStateService} from "./signalState.service";
     <h2>Writable Signals</h2>
     <div>
       <p>Names: {{ names() }}</p>
+      <p>Names and Surnames: {{ namesAndSurnames }}</p>
       <input #inputElement (input)="onSetInput(inputElement.value)" type="text">
       <br>
-      <input placeholder=" - " #inputElement (input)="onUpdateInput(inputElement.value)" type="text">
+      <input #secondInputElement placeholder=" - " #inputElement (input)="onUpdateInput(secondInputElement.value)" type="text">
     </div>
   `,
   styles: ``,
@@ -21,9 +22,11 @@ import {SignalStateService} from "./signalState.service";
 })
 export class SignalsTesterComponent {
   public names!: Signal<string>;
+  public namesAndSurnames!: Signal<string>;
 
   constructor(private signalStateService: SignalStateService) {
     this.names = this.signalStateService.names;
+    this.namesAndSurnames = this.signalStateService.namesAndSurnames;
   }
 
   public onSetInput(value: string): void {
