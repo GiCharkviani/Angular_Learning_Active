fd2b8a57665595a788ec6a0d2754732330f46944
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-08 13:25:36 +0100

    gfdgd

--- ActiveLearningPractice/learning-practice/src/app/signals/signalsTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/signals/signalsTester.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, signal} from "@angular/core";
 
 @Component({
   selector: 'app-signalsTester',
@@ -7,11 +7,21 @@ import {Component} from "@angular/core";
 
   ],
   template: `
+    <h2>Writable Signals</h2>
+    <div>
+      <p>Names: {{ names() }}</p>
+      <input (input)="onInput()" type="text">
+    </div>
   `,
   styles: ``
 })
 export class SignalsTesterComponent {
   /* Writable Signals */
-  
+  public names = signal(null);
+
+  public onInput(): void {
+    
+  }
+
 
 }
