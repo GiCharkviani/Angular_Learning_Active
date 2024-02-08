d50bfe65f24778dd109a60e053422ebac74e4d63
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-08 18:12:50 +0100

    gfdgdgd

--- ActiveLearningPractice/learning-practice/src/app/signals/signalState.service.ts
+++ ActiveLearningPractice/learning-practice/src/app/signals/signalState.service.ts
@@ -1,18 +1,24 @@
 import {computed, Injectable, signal} from "@angular/core";
 
 
-function equality(value: any) {
-  console.log(value, 'FROM_EQUAL')
+function equality(value1: string, value2: string): boolean {
+  return value1 === value2;
 }
+
+const animal = equality
 
 @Injectable({providedIn: 'root'})
 export class SignalStateService {
   /* Writable Signals */
-  public names = signal('', {
-    equal:
+  public names = signal<string>('', {
+    equal: equality
   });
 
   public surnames = signal('');
+
+  constructor() {
+    console.dir(animal, 'RAGAC')
+  }
 
   /* Readable Signals */
   public namesAndSurnames = computed(() => {
