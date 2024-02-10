5fc9a6c4d0f8b221f794d69457568d844262f0c3
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-10 13:50:09 +0100

    fdsfsf

--- ActiveLearningPractice/learning-practice/src/app/routing/routerTester.component.ts
+++ ActiveLearningPractice/learning-practice/src/app/routing/routerTester.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -6,9 +7,23 @@ import {Component} from "@angular/core";
   standalone: true,
   imports: [],
   template: `
+    <button (click)="toSelector()">To Selector</button>
   `,
   styles: ``
 })
 export class RouterTesterComponent {
+
+  constructor(private readonly router: Router) {
+    /* Router Native */
+    // console.log(window.location, 'LOCATION')
+    // console.log(window.history)
+    // window.history.pushState({}, '', 'gaga')
+  }
+
+  public toSelector(): void {
+    this.router.navigate(['selector'])
+  }
+
+
 
 }
